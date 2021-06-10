2158890f94308bddbcece35f08b1969b44cb73b6
nmerey <merey.nurlan@gmail.com>
2021-06-10 19:26:58 +0600

    check status of property using booking id

--- lib/booking_service.rb
+++ lib/booking_service.rb
@@ -4,10 +4,21 @@ class BookingService
 		@events = events
 	end
 
-	def check_status
-		last_event = @events.filter{ |event| event.data.property_id == @property.id }.sort_by { |event| event.created_at }.last
-		if last_event
-			true
+	def bookings
+		@events.filter{ |event| event.data["property_id"] == @property.id }
+	end
+
+	def booking_id
+		if !self.bookings.empty?
+			self.bookings.first.data["booking_id"]
 		end
+	end
+
+	def property_events
+		@events.filter{ |event| event.data["booking_id"] == self.booking_id }
+	end
+	
+	def check_status
+		self.property_events.empty? || self.property_events.last.type == "BookingCheckedOut" ? "empty" : "occupied"
 	end
 end
--- spec/booking_service_spec.rb
+++ spec/booking_service_spec.rb
@@ -1,15 +1,20 @@
 require 'repositories/property_repository'
 require 'repositories/event_repository'
 require 'booking_service'
+require 'byebug'
+
 RSpec.describe BookingService do
-	let(:properties) { PropertyRepository.new(source: JsonReader.new('data/properties.json', 'properties')) }
-	let(:events) { EventRepository.new(source: JsonReader.new('data/events.json', 'events')) }
+	let(:properties) { PropertyRepository.new(source: JsonReader.new('data/properties.json', 'properties')).all }
+	let(:events) { EventRepository.new(source: JsonReader.new('data/events.json', 'events')).all }
 	let(:booking_first) { BookingService.new( properties.first, events ) }
-	let(:booking_last) { BookingService.new( properties.last, events ) }
+	let(:booking_last) { BookingService.new( properties.last , events ) }
 
-
+	it "should return all events related to property" do
+		expect(booking_first.property_events).to eq(events.filter{ |event| event.data["booking_id"] == "aaa" })
+	end
+	
 	it "should return status of property" do
 		expect(booking_first.check_status).to eq('empty')
-		expect(booking_last.check_status).to eq('occupied')
+		expect(booking_last.check_status).to eq('empty')
 	end
 end
